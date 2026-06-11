3ab9e707c264887f227733e6003e6b97dacda834
zoukehui <zoukehui@qq.com>
2026-06-11 18:34:33 +0800

    update: 登录问题

--- whyisee-community/app/astro.config.mjs
+++ whyisee-community/app/astro.config.mjs
@@ -9,5 +9,17 @@ export default defineConfig({
     enabled: false,
   },
   output: "server",
+  security: {
+    allowedDomains: [
+      {
+        hostname: "whyisee.xyz",
+        protocol: "https",
+      },
+      {
+        hostname: "www.whyisee.xyz",
+        protocol: "https",
+      },
+    ],
+  },
   site: process.env.SITE_URL || "https://whyisee.xyz",
 });
